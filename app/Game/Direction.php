20ea730786a7d576677e59d13042633d1c3742c9
Hunter Skrasek <hskrasek@modernize.com>
2022-02-24 16:00:35 -0600

    Reorganize the application

---
+++ app/Game/Direction.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+enum Direction: string
+{
+    case North = 'North';
+    case NorthEast = 'North-East';
+    case NorthWest = 'North-West';
+    case East = 'East';
+    case South = 'South';
+    case SouthEast = 'South-East';
+    case SouthWest = 'South-West';
+    case West = 'West';
+
+    public function toCardinality(): array
+    {
+        return match ($this) {
+            self::North => [-1, 0],
+            self::NorthEast => [-1, 1],
+            self::NorthWest => [-1, -1],
+            self::East => [0, 1],
+            self::South => [1, 0],
+            self::SouthEast => [1, 1],
+            self::SouthWest => [1, -1],
+            self::West => [0, -1],
+        };
+    }
+}
---
+++ tests/Unit/Game/GridTest.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Direction;
+use App\GameGrid;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
+
+// beforeAll(function () {
+//
+// });
+
+it('can insert a word into the grid in any direction', function (string $direction) {
+    $word = 'APPLE';
+
+    $grid = new GameGrid(15);
+    $grid->insertWord($word, Direction::from($direction));
+
+    expect((string)$grid)->toBeString()->toContain(...Str::ucsplit($word));
+})->with('directions');
+
+it('can insert multiple words in any direction', function () {
+    $grid = new GameGrid(15);
+    $directions = Direction::cases();
+    shuffle($directions);
+
+    $grid->insertWord('APPLE', Arr::random($directions));
+
+    $grid->insertWord('UNMARRED', Arr::random($directions));
+
+    $grid->insertWord('OXYTOCIN', Arr::random($directions));
+
+    $grid->insertWord('HALO', Arr::random($directions));
+
+    $gridString = (string)$grid;
+
+    expect($gridString)->toBeString()->toContain(...Str::ucsplit('APPLE'));
+    expect($gridString)->toBeString()->toContain(...Str::ucsplit('UNMARRED'));
+    expect($gridString)->toBeString()->toContain(...Str::ucsplit('OXYTOCIN'));
+    expect($gridString)->toBeString()->toContain(...Str::ucsplit('HALO'));
+});
