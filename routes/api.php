d6fcec69d862a44a636b4ee7709780488464399d
Hunter Skrasek <hskrasek@modernize.com>
2022-04-06 10:05:37 -0500

    Adding some API endpoints for solving/finding words, and retrieving game information

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Game\Difficulty;
+use App\Http\Resources\GameResource;
+use App\Models\Game;
+use App\Models\Word;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +21,47 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('games/{game}', function (Request $request, Game $game) {
+    return new GameResource($game);
+});
+
+Route::post('games', function (Request $request) {
+    $difficulty = Difficulty::from($request->input('difficulty'));
+
+    //TODO: Grid creation is fucked up, not sure why. Might need to revert the entire Grid class changes
+    //TODO: Need to improve the word selection based off of the difficulty, current approach I think is broken
+
+    $words = Word::query()
+        ->whereBetween('length', [$difficulty->gridSize()-2, $difficulty->gridSize()])
+        ->inRandomOrder()
+        ->limit($difficulty->wordCount())
+        ->get();
+
+    $game = Game::start($difficulty, $words);
+
+    return new GameResource($game);
+});
+
+Route::post('/games/{game}/solve', function (Request $request, Game $game) {
+    if (!$game->words->contains($word = Word::where('text', $request->word)->first())) {
+        return \response([], 422);
+    }
+
+    $wordCoordinates = $game->grid->getWordCoordinates()[$word->text];
+
+    $solved = false;
+
+    foreach ($wordCoordinates as $index => $wordCoordinate) {
+        $solved = $solved || $wordCoordinate === $request->coordinates[$index];
+    }
+
+    if (!$solved) {
+        return \response([], 422);
+    }
+
+    $game->words()->updateExistingPivot($word, ['found' => true, 'found_at' => Carbon::now()]);
+
+    return Redirect::route('game.play', [$game->uuid]);
 });
