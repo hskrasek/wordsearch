1fc5b82ffa2637d565df1a750fc4128796322ad3
Hunter Skrasek <hskrasek@modernize.com>
2022-04-06 10:05:16 -0500

    Update WordSeeder for now to be simple

--- database/seeders/WordSeeder.php
+++ database/seeders/WordSeeder.php
@@ -5,8 +5,6 @@ namespace Database\Seeders;
 use App\Models\Word;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\LazyCollection;
-use Illuminate\Support\Str;
 
 class WordSeeder extends Seeder
 {
@@ -19,7 +17,108 @@ class WordSeeder extends Seeder
      */
     public function run()
     {
-        $words = LazyCollection::make($this->container->make('words'));
-        $words->keys()->each(fn(string $word) => Word::create(['text' => Str::upper($word)]));
+        // $words = LazyCollection::make($this->container->make('words'));
+        // $words->keys()->each(fn(string $word) => Word::create(['text' => Str::upper($word)]));
+        Word::create(
+            [
+                'text' => 'APPLE'
+            ]
+        );
+
+        Word::create(
+            [
+                'text' => 'HALO'
+            ]
+        );
+
+        Word::create(
+            [
+                'text' => 'DESTINY'
+            ]
+        );
+
+        Word::create(
+            [
+                'text' => 'GUARDIAN'
+            ]
+        );
+
+        Word::create(
+            [
+                'text' => 'WITNESS'
+            ]
+        );
+
+        Word::create(
+            [
+                'text' => 'LIGHT'
+            ]
+        );
+
+        Word::create(
+            [
+                'text' => 'DARKNESS'
+            ]
+        );
+
+        Word::create(
+            [
+                'text' => 'SCORN'
+            ]
+        );
+
+        Word::create(
+            [
+                'text' => 'HIVE'
+            ]
+        );
+
+        Word::create(
+            [
+                'text' => 'LOVE'
+            ]
+        );
+
+        Word::create(
+            [
+                'text' => 'ENTER'
+            ]
+        );
+
+        Word::create(
+            [
+                'text' => 'GIVE'
+            ]
+        );
+
+        Word::create(
+            [
+                'text' => 'COMMUNE'
+            ]
+        );
+
+        Word::create(
+            [
+                'text' => 'KILL'
+            ]
+        );
+
+        Word::create(
+            [
+                'text' => 'TRAVELLER'
+            ]
+        );
+
+        Word::create(
+            [
+                'text' => 'PYRAMID'
+            ]
+        );
+
+        Word::create(
+            [
+                'text' => 'ASCENDANT'
+            ]
+        );
     }
 }
