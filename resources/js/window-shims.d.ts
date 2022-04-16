ffae478907001e30000093232bece05c7adba112
Hunter Skrasek <hskrasek@modernize.com>
2022-04-16 01:00:28 -0500

    Remove redundant ziggy type definition/shims

--- resources/js/window-shims.d.ts
+++ resources/js/window-shims.d.ts
@@ -1,18 +1,9 @@
 import { LoDashStatic } from "lodash";
 import { AxiosInstance } from "axios";
-import { Config, RouteParamsWithQueryOverload, Router } from "ziggy-js";
 
 declare global {
     interface Window {
         _: LoDashStatic;
         axios: AxiosInstance;
     }
-
-    declare function route(): Router;
-    declare function route(
-        name: string,
-        params?: RouteParamsWithQueryOverload,
-        absolute?: boolean,
-        customZiggy?: Config
-    ): string;
 }
