1796a52e5aea504cc7cc8dd101e21be303005ce1
Hunter Skrasek <hskrasek@modernize.com>
2022-04-23 23:57:19 -0500

    Cleanup home page and add word from the author

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -18,16 +18,19 @@ function startGame() {
     <Head title="Start Game" />
 
     <div class="grid grid-cols-1 items-center">
-        <div class="sm:px-6 lg:px-8">
-            <h1 class="text-center text-3xl font-bold">Choose Difficulty</h1>
+        <div class="max-w-lg sm:px-6 lg:px-8">
+            <h1 class="text-center text-3xl font-bold">New Game</h1>
+            <h2 class="mt-3 text-center text-2xl font-bold">
+                Choose Difficulty:
+            </h2>
             <form @submit.prevent="startGame">
-                <div>
+                <div class="w-full">
                     <button
                         v-for="(difficulty, i) in difficulties"
                         :key="i"
                         type="button"
-                        :value="difficulty.value"
-                        class="inline-block bg-blue-500 px-6 py-2.5 text-lg font-semibold uppercase leading-tight text-white transition duration-150 ease-in-out hover:bg-blue-600 focus:bg-blue-600 focus:outline-none focus:ring-0 active:bg-blue-800"
+                        :value="difficulty.name"
+                        class="inline-block bg-blue-500 px-[1.640rem] py-2.5 text-lg font-semibold uppercase leading-tight text-white transition duration-150 ease-in-out hover:bg-blue-600 focus:bg-blue-600 focus:outline-none focus:ring-0 active:bg-blue-800"
                         :class="{
                             'rounded-l': i === 0,
                             'rounded-r': i === difficulties.length - 1,
@@ -47,6 +50,16 @@ function startGame() {
                     </button>
                 </div>
             </form>
+            <h2 class="mt-6 text-xl font-semibold">A word from the author,</h2>
+            <p class="break-words font-normal">
+                I hope you enjoy solving word search puzzles on my little side
+                project. I want to keep this project active and improve it along
+                the way as I learn more. A nicer design, mobile friendliness,
+                and more is on the horizon, but I wanted to get this out in the
+                open as soon as possible. Pardon my dust while I keep making
+                improvements.
+            </p>
+            <p class="font-thin">- Hunter Skrasek</p>
         </div>
     </div>
 </template>
