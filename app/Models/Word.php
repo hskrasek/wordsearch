9f5a7484ead0abdda11c253b057ce69629d481a5
Hunter Skrasek <hunterskrasek@me.com>
2022-05-01 02:03:47 -0500

    Initial pass at new difficulty based word selection

--- app/Models/Word.php
+++ app/Models/Word.php
@@ -6,6 +6,8 @@ use App\Game\Difficulty;
 use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Staudenmeir\LaravelCte\Eloquent\QueriesExpressions;
+use Staudenmeir\LaravelCte\Query\Builder as CTEBuilder;
 
 /**
  * App\Models\Word
@@ -29,22 +31,30 @@ use Illuminate\Database\Eloquent\Model;
  * @method static \Illuminate\Database\Eloquent\Builder|Word whereFrequency($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Word difficulty(\App\Game\Difficulty $difficulty)
  * @method static \Illuminate\Database\Eloquent\Builder|Word excludeWords(\App\Models\Word ...$word)
+ * @method static \Illuminate\Database\Eloquent\Builder|Word leastFrequentByDifficulty(\App\Game\Difficulty $difficulty)
+ * @method static \Illuminate\Database\Eloquent\Builder|Word mostFrequentByDifficulty(\App\Game\Difficulty $difficulty)
  */
 class Word extends Model
 {
     use HasFactory;
+    use QueriesExpressions;
 
     public function scopeDifficulty(Builder $query, Difficulty $difficulty): Builder
     {
         return $query->where('length', '>=', $difficulty->minimumWordLength())
             ->where('length', '<=', $difficulty->gridSize())
+            ->whereNot($query->raw('LOWER("text")'), '~', 'w{3}.*')
             ->when($difficulty === Difficulty::Easy, fn(Builder $builder) => $builder->orderByDesc('frequency'))
-            ->when($difficulty === Difficulty::Medium)
-            ->when($difficulty === Difficulty::Hard)
+            ->when(
+                ($difficulty === Difficulty::Medium || $difficulty === Difficulty::Hard),
+                fn(Builder|CTEBuilder|Word $builder) => $builder->leastFrequentByDifficulty($difficulty)->unionAll(
+                    $builder->mostFrequentByDifficulty($difficulty)
+                )
+            )
             ->when($difficulty === Difficulty::Insane, function (Builder $builder) {
-                return $builder->whereNot('text', '~', '[A-Z]*[AEIOUY]{2,}[A-Z]*')
-                    ->whereNot('text', '~', 'W{3}.*')
-                    ->whereNot('text', '~', '[B-DF-JH-NP-TV-Z]{2,}')
+                return $builder->whereNot($builder->raw('LOWER("text")'), '~', '[a-z]*[aeiouy]{2,}[a-z]*')
+                    ->whereNot($builder->raw('LOWER("text")'), '~', '[b-df-jh-np-tv-z]{2,}')
+                    ->whereNot($builder->raw('LOWER("text")'), '~', '[a-z]o[a-z]+')
                     ->orderBy('frequency');
             })
             ->limit($difficulty->wordCount());
@@ -53,5 +63,39 @@ class Word extends Model
     public function scopeExcludeWords(Builder $query, Word ...$word): Builder
     {
         return $query->whereNotIn('text', array_map(static fn(Word $word) => $word->text, $word));
+    }
+
+    public function scopeLeastFrequentByDifficulty(Builder|CTEBuilder $query, Difficulty $difficulty): Builder
+    {
+        return $query->newQuery()->from('least_frequent_words')
+            ->withExpression(
+                'least_frequent_words',
+                $query->from($this->getTable())->where('length', '>=', $difficulty->minimumWordLength())
+                    ->where('length', '<=', $difficulty->gridSize())
+                    ->whereNot($query->raw('LOWER("text")'), '~', '[a-z]*[aeiouy]{2,}[a-z]*')
+                    ->whereNot($query->raw('LOWER("text")'), '~', 'w{3}.*')
+                    ->whereNot($query->raw('LOWER("text")'), '~', '[b-df-jh-np-tv-z]{2,}')
+                    ->whereNot($query->raw('LOWER("text")'), '~', '[a-z]o[a-z]+')
+                    ->inRandomOrder()
+                    ->orderBy('frequency')
+                    ->limit($difficulty->wordCount() / 2)
+            );
+    }
+
+    public function scopeMostFrequentByDifficulty(Builder|CTEBuilder $query, Difficulty $difficulty): Builder
+    {
+        return $query->newQuery()->from('most_frequent_words')
+            ->withExpression(
+                'most_frequent_words',
+                $query->from($this->getTable())->where('length', '>=', $difficulty->minimumWordLength())
+                    ->where('length', '<=', $difficulty->gridSize())
+                    ->whereNot($query->raw('LOWER("text")'), '~', '[a-z]*[aeiouy]{2,}[a-z]*')
+                    ->whereNot($query->raw('LOWER("text")'), '~', 'w{3}.*')
+                    ->whereNot($query->raw('LOWER("text")'), '~', '[b-df-jh-np-tv-z]{2,}')
+                    ->whereNot($query->raw('LOWER("text")'), '~', '[a-z]o[a-z]+')
+                    ->inRandomOrder()
+                    ->orderByDesc('frequency')
+                    ->limit($difficulty->wordCount() / 2)
+            );
     }
 }
