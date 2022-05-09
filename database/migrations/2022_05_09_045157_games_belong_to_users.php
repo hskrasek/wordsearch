dc9028b47722b86db18201feccf0a2e929a94dc0
Hunter Skrasek <hunterskrasek@me.com>
2022-05-09 00:28:08 -0500

    Update users table to allow "anonymous" users. Update games to belong to users

---
+++ database/migrations/2022_05_09_045157_games_belong_to_users.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::disableForeignKeyConstraints();
+
+        DB::table('game_word')->truncate();
+        DB::table('games')->truncate();
+
+        Schema::enableForeignKeyConstraints();
+
+        Schema::table('games', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id')->after('id');
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->cascadeOnDelete()
+                ->deferrable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('games', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+
+            $table->dropColumn('user_id');
+        });
+    }
+};
