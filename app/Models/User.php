b33abdc2270aedb761299e79dcf49b6faa1093ef
Hunter Skrasek <hunterskrasek@me.com>
2022-05-11 22:34:37 -0500

    Update User model to add attribute for determining if a user is anonymous or not

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -55,6 +56,10 @@ class User extends Authenticatable
         'password',
     ];
 
+    protected $appends = [
+        'is_anonymous'
+    ];
+
     /**
      * The attributes that should be hidden for serialization.
      *
@@ -73,6 +78,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function isAnonymous(): Attribute
+    {
+        return Attribute::make(
+            get: fn() => empty($this->email) && empty($this->email_verified_at),
+        );
+    }
 
     public function games(): HasMany
     {
