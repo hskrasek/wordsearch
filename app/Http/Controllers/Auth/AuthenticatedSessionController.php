5dac0420e5855ae26b02c235b8bfb5e1006408d8
Hunter Skrasek <hunterskrasek@me.com>
2022-05-15 01:32:46 -0500

    Redirect login requests back to home

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -32,7 +32,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->back();
+        return redirect()->intended(RouteServiceProvider::HOME);
     }
 
     /**
