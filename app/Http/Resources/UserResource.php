9851079084afdc6b8953c3750645311a19136b62
Hunter Skrasek <hunterskrasek@me.com>
2022-05-15 01:29:07 -0500

    Testing and user API response data updates

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -16,6 +16,7 @@ class UserResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'name'         => $this->name,
             'username'     => $this->username,
             'is_anonymous' => $this->is_anonymous,
             'created_at'   => $this->created_at,
