0d4553c02d1fd4b63ecc9ed9810649e3110c77f2
Hunter Skrasek <hunterskrasek@me.com>
2022-05-28 00:23:31 -0500

    Add Hyrule and use it in new EditUser form request

---
+++ app/Http/Requests/EditUser.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Square\Hyrule\Hyrule;
+use CountryState;
+
+class EditUser extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $builder = Hyrule::create();
+
+        $builder->string('birthday')
+            ->sometimes()
+            ->required()
+            ->rule('date')
+            ->lte(now()->toDateString())
+            ->end();
+
+        $builder->string('country')
+            ->sometimes()
+            ->requiredWith('state')
+            ->in(array_keys(CountryState::getCountries()));
+
+        $builder->string('state')
+            ->sometimes()
+            ->requiredWith('country')
+            ->in(array_keys(CountryState::getStates($this->country)));
+
+        return $builder->build();
+    }
+}
