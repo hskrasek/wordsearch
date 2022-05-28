073da51f6bb7c7ba91e83b86d4f33c460c1b18bf
Hunter Skrasek <hunterskrasek@me.com>
2022-05-28 00:50:59 -0500

    Seed initial permissions

---
+++ database/seeders/PermissionsSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\PermissionRegistrar;
+
+class PermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Reset cached roles and permissions
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        Permission::create(['name' => 'view profile']);
+        Permission::create(['name' => 'edit profile']);
+        Permission::create(['name' => 'play games']);
+    }
+}
