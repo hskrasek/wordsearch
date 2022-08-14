c888676a4beeb25fe2d46f8ff5c1b8d7a89c4fe5
Hunter Skrasek <hunterskrasek@me.com>
2022-08-13 23:08:10 -0500

    Cleanup Vite setup and Auth pages

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -4,7 +4,7 @@ import BreezeInput from "@/Components/Input.vue";
 import BreezeLabel from "@/Components/Label.vue";
 import BreezeValidationErrors from "@/Components/ValidationErrors.vue";
 import { Head, useForm } from "@inertiajs/inertia-vue3";
-import route from "@/ziggy";
+import route from "ziggy";
 
 const form = useForm({
     email: "",
