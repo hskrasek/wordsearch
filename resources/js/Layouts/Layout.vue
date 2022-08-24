8f0d91ac7a64dcd03de35b88d995bb59b5f10da8
Hunter Skrasek <hunterskrasek@me.com>
2022-08-23 23:35:47 -0500

    Remove search bar, and comment out the status property
    Signed-off-by: Hunter Skrasek <hunterskrasek@me.com>

--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -34,7 +34,7 @@ const props = defineProps<{
 }>();
 
 const user = computed(() => props.auth.user);
-const status = computed(() => props.status);
+// const status = computed(() => props.status);
 
 const navigation = [
     { name: "Home", href: route("home"), current: route().current("home") },
@@ -120,34 +120,6 @@ const userNavigation = [
                         </Menu>
                     </div>
 
-                    <!-- Search -->
-                    <div class="min-w-0 flex-1 px-12 lg:hidden">
-                        <div class="mx-auto w-full max-w-xs">
-                            <label for="desktop-search" class="sr-only"
-                                >Search</label
-                            >
-                            <div
-                                class="relative text-white focus-within:text-gray-600"
-                            >
-                                <div
-                                    class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3"
-                                >
-                                    <SearchIcon
-                                        class="h-5 w-5"
-                                        aria-hidden="true"
-                                    />
-                                </div>
-                                <input
-                                    id="desktop-search"
-                                    class="block w-full rounded-md border border-transparent bg-white bg-opacity-20 py-2 pl-10 pr-3 leading-5 text-gray-900 placeholder-white focus:border-transparent focus:bg-opacity-100 focus:placeholder-gray-500 focus:outline-none focus:ring-0 sm:text-sm"
-                                    placeholder="Search"
-                                    type="search"
-                                    name="search"
-                                />
-                            </div>
-                        </div>
-                    </div>
-
                     <!-- Menu button -->
                     <div class="absolute right-0 flex-shrink-0 lg:hidden">
                         <!-- Mobile menu button -->
@@ -191,32 +163,6 @@ const userNavigation = [
                                     {{ item.name }}
                                 </a>
                             </nav>
-                        </div>
-                        <div>
-                            <div class="mx-auto w-full max-w-md">
-                                <label for="mobile-search" class="sr-only"
-                                    >Search</label
-                                >
-                                <div
-                                    class="relative text-white focus-within:text-gray-600"
-                                >
-                                    <div
-                                        class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3"
-                                    >
-                                        <SearchIcon
-                                            class="h-5 w-5"
-                                            aria-hidden="true"
-                                        />
-                                    </div>
-                                    <input
-                                        id="mobile-search"
-                                        class="block w-full rounded-md border border-transparent bg-white bg-opacity-20 py-2 pl-10 pr-3 leading-5 text-gray-900 placeholder-white focus:border-transparent focus:bg-opacity-100 focus:placeholder-gray-500 focus:outline-none focus:ring-0 sm:text-sm"
-                                        placeholder="Search"
-                                        type="search"
-                                        name="search"
-                                    />
-                                </div>
-                            </div>
                         </div>
                     </div>
                 </div>
