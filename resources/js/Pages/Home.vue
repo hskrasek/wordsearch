a3cbc3c6a92c75587717ecddf3d405cd873277cb
Hunter Skrasek <hunterskrasek@me.com>
2022-08-25 00:05:08 -0500

    Update the home page to implement the new layout and a new design
    Signed-off-by: Hunter Skrasek <hunterskrasek@me.com>

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,18 +1,46 @@
 <script setup lang="ts">
-import { Head, useForm } from "@inertiajs/inertia-vue3";
+import { PlayIcon } from "@heroicons/vue/outline";
+import { Head, Link } from "@inertiajs/inertia-vue3";
 import Layout from "@/Layouts/Layout.vue";
+import { computed } from "vue";
+import route from "ziggy";
 
-defineProps<{
+const props = defineProps<{
     difficulties: Array<{ value: number; name: string }>;
 }>();
 
-const newGame = useForm({
-    difficulty: "",
-});
+const difficultyColorMap = {
+    Easy: {
+        iconForeground: "text-lime-700",
+        iconBackground: "bg-lime-50",
+    },
+    Medium: {
+        iconForeground: "text-teal-700",
+        iconBackground: "bg-teal-50",
+    },
+    Hard: {
+        iconForeground: "text-pink-700",
+        iconBackground: "bg-pink-50",
+    },
+    Insane: {
+        iconForeground: "text-rose-700",
+        iconBackground: "bg-rose-50",
+    },
+};
 
-function startGame() {
-    newGame.post("/game");
-}
+const actions = computed(() => {
+    return props.difficulties.map((difficulty) => {
+        let colors = difficultyColorMap[difficulty.name];
+
+        return {
+            title: difficulty.name,
+            href: route("game.create"),
+            icon: PlayIcon,
+            iconForeground: colors.iconForeground,
+            iconBackground: colors.iconBackground,
+        };
+    });
+});
 </script>
 
 <template>
@@ -22,43 +50,81 @@ function startGame() {
 
     <Layout>
         <template #content>
-            <!--            <div class="grid grid-cols-1 items-center">-->
-            <div class="sm:px-6 lg:px-8">
-                <h1 class="text-center text-3xl font-bold dark:text-white">
-                    New Game
-                </h1>
-                <h2 class="mt-3 text-center text-2xl font-bold dark:text-white">
-                    Choose Difficulty:
-                </h2>
-                <form @submit.prevent="startGame">
-                    <div class="w-full">
-                        <button
-                            v-for="(difficulty, i) in difficulties"
-                            :key="i"
-                            type="button"
-                            :value="difficulty.name"
-                            class="inline-block bg-blue-500 px-[0.813rem] py-2.5 text-lg font-semibold uppercase leading-tight text-white transition duration-150 ease-in-out hover:bg-blue-600 focus:bg-blue-600 focus:outline-none focus:ring-0 active:bg-blue-800 dark:bg-violet-500 dark:hover:bg-violet-600 dark:focus:bg-violet-600 dark:active:bg-violet-800 md:px-[1.640rem] md:py-2.5 lg:px-[1.640rem] lg:py-2.5"
-                            :class="{
-                                'rounded-l': i === 0,
-                                'rounded-r': i === difficulties.length - 1,
-                            }"
-                            @click="newGame.difficulty = difficulty.name"
+            <div
+                class="divide-y divide-gray-200 overflow-hidden rounded-lg bg-gray-200 shadow sm:grid sm:grid-cols-2 sm:gap-px sm:divide-y-0"
+            >
+                <div
+                    v-for="(action, actionIdx) in actions"
+                    :key="action.title"
+                    :class="[
+                        actionIdx === 0
+                            ? 'rounded-tl-lg rounded-tr-lg sm:rounded-tr-none'
+                            : '',
+                        actionIdx === 1 ? 'sm:rounded-tr-lg' : '',
+                        actionIdx === actions.length - 2
+                            ? 'sm:rounded-bl-lg'
+                            : '',
+                        actionIdx === actions.length - 1
+                            ? 'rounded-bl-lg rounded-br-lg sm:rounded-bl-none'
+                            : '',
+                        'group relative bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500',
+                    ]"
+                >
+                    <div>
+                        <span
+                            :class="[
+                                action.iconBackground,
+                                action.iconForeground,
+                                'inline-flex rounded-lg p-3 ring-4 ring-white',
+                            ]"
                         >
-                            {{ difficulty.name }}
-                        </button>
+                            <component
+                                :is="action.icon"
+                                class="h-6 w-6"
+                                aria-hidden="true"
+                            />
+                        </span>
                     </div>
-                    <div class="w-full">
-                        <button
-                            type="submit"
-                            class="mt-1.5 inline-block w-full bg-green-300 px-6 py-2.5 text-lg font-semibold uppercase leading-tight text-white hover:bg-green-400 dark:bg-teal-300 dark:hover:bg-teal-400"
-                            :disabled="newGame.processing"
+                    <div class="mt-8">
+                        <h3 class="text-lg font-medium">
+                            <Link
+                                :href="action.href"
+                                class="focus:outline-none"
+                                as="button"
+                                method="post"
+                                :data="{ difficulty: action.title }"
+                            >
+                                <!-- Extend touch target to entire panel -->
+                                <span
+                                    class="absolute inset-0"
+                                    aria-hidden="true"
+                                />
+                                {{ action.title }}
+                            </Link>
+                        </h3>
+                        <p class="mt-2 text-sm text-gray-500">
+                            Doloribus dolores nostrum quia qui natus officia
+                            quod et dolorem. Sit repellendus qui ut at
+                            blanditiis et quo et molestiae.
+                        </p>
+                    </div>
+                    <span
+                        class="pointer-events-none absolute top-6 right-6 text-gray-300 group-hover:text-gray-400"
+                        aria-hidden="true"
+                    >
+                        <svg
+                            class="h-6 w-6"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor"
+                            viewBox="0 0 24 24"
                         >
-                            Start Game
-                        </button>
-                    </div>
-                </form>
+                            <path
+                                d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z"
+                            />
+                        </svg>
+                    </span>
+                </div>
             </div>
-            <!--            </div>-->
         </template>
 
         <template #sidebar>
