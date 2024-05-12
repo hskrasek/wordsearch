b3072325383819c29e35dc27ae37791c20ded593
Shift <shift@laravelshift.com>
2024-05-12 04:17:47 +0000

    Remove explicit call to register policies

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -24,8 +24,6 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->registerPolicies();
-
         Gate::before(function (User $user, $ability) {
             return $user->hasRole('Super Admin') ? true : null;
         });
